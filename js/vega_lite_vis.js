06800965c43e8f7ec2b7704eac803ab3361150f1
ayonnasaha <116136895+ayonnasaha@users.noreply.github.com>
2024-10-07 00:50:38 +1100

    Update vega_lite_vis.js

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,6 +1,10 @@
-var vg_1 = "migrant_population_choropleth.vg.json";
+var vg_1 = "js/area_chart.vg.json";
+var vg_2="js/migrant_population_choropleth.vg.json"
 
-vegaEmbed("#choropleth", vg_1).then(function(result) {
+vegaEmbed("#area_chart", vg_1).then(function(result) {
 // Access the Vega view instance
 // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+vegaEmbed("#choropleth", vg_2).then(function(result) {
+    }).catch(console.error);
