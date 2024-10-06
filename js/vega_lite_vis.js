2ba69a4facbce354f5e0825b3bbc29e90261080b
ayonnasaha <116136895+ayonnasaha@users.noreply.github.com>
2024-10-06 22:08:55 +1100

    Update vega_lite_vis.js

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,1 +1,6 @@
+var vg_1 = "migrant_population_choropleth.vg.json";
 
+vegaEmbed("#choropleth", vg_1).then(function(result) {
+// Access the Vega view instance
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
